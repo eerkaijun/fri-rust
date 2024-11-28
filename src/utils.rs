9b9a71d1fb09c81bd212f9e910d9b0a69880c75f
eerkaijun <kaijuneer@gmail.com>
2024-11-28 14:44:43 +0000

    test case for foly_polynomial

--- src/utils.rs
+++ src/utils.rs
@@ -18,3 +18,34 @@ pub fn fold_polynomial<E: Field>(poly: &[E], random_value: E) -> Vec<E> {
         .collect();
     folded_poly
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use ark_bls12_381::Fq2 as F;
+
+    // TODO: figure out how to not use BLS curve
+    #[test]
+    fn test_fold_polynomial() {
+        // create a simple polynomial with coefficients in Fp64
+        let poly: Vec<F> = vec![
+            F::from(1u32), // x^0
+            F::from(2u32), // x^1 
+            F::from(3u32), // x^2
+            F::from(4u32), // x^3
+        ];
+        
+        let random_value = F::from(5u32);
+        
+        let folded = fold_polynomial(&poly, random_value);
+        
+        // for polynomial 1 + 2x + 3x^2 + 4x^3
+        // even coefficients are [1, 3]
+        // odd coefficients are [2, 4]
+        // after folding with random value r:
+        // result should be [(1 + 5*2), (3 + 5*4)]
+        assert_eq!(folded.len(), 2);
+        assert_eq!(folded[0], F::from(11u32));
+        assert_eq!(folded[1], F::from(23u32));
+    }
+}
